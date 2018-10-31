7f73216b149cfdf242dff36ba120c298008fd702
liligeng <lilg1@ifeng.com>
2018-10-31 15:53:07 +0800

    first commit

---
+++ weitter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 1.6
+ */
+/*
+Plugin Name: weitter
+Plugin URI: http://wordpress.org/plugins/weitter/
+Description: A weibo and twitter like plugin to help create your own feed.
+Author: liligeng
+Version: 1.6
+Author URI: http://liligeng.com/
+*/
+
+?>
